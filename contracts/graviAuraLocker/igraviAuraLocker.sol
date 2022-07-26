2edb2135e14e1ad65ad28c25eca108e62e41dae5
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-27 00:43:00 +0430

    graviauralocker interface added

---
+++ contracts/graviAuraLocker/igraviAuraLocker.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/**
+ * @title   AuraLocker
+ * @author  Aj Maz
+ * @notice  Interface for igraviAuraLocker
+ *          There will be two version of this locker.
+ *          One would be for the protocol owns tokens and one would be allow protocols to reward their users for staking their tokens.
+ *          This interface focuses on the first one but the second one must also support this interface as well.
+ *          This locker is highly inspired by CVX Locking contract
+ *
+ */
+interface igraviAuraLocker {
+    /*********** STRUCTS ***********/
+    struct Deposits {
+        uint256 amount;
+        uint256 withdrawAmount;
+        uint32 unlockTime;
+    }
+    struct Epoch {
+        uint224 supply;
+        uint32 date;
+    }
+
+    /*********** ADMIN ***********/
+    function recoverERC20(address _tokenAddress, uint256 _tokenAmount) external;
+
+    /*********** ACTIONS ***********/
+    function lock(address _account, uint256 _amount) external;
+
+    function checkpointEpoch() external;
+
+    function withdraw() external;
+
+    /*********** VIEWS ***********/
+    function balanceOf(address _user) external;
+
+    function withdrawableBalanceOf(address _user) external;
+
+    function deposits(address _user)
+        external
+        returns (Deposits[] memory deposits);
+
+    function totalSupply() external view returns (uint256 supply);
+
+    function totalSupplyAtEpoch(uint256 _epoch)
+        external
+        view
+        returns (uint256 supply);
+
+    function epochCount() external view returns (uint256);
+}
