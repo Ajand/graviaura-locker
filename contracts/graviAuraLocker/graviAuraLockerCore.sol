6a156ba3c134a6c044ae8be3f4dc7506b1316b82
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-27 02:37:00 +0430

    base of graviauralockerCore added

---
+++ contracts/graviAuraLocker/graviAuraLockerCore.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {igraviAuraLocker} from "./igraviAuraLocker.sol";
+
+/**
+ * @title   AuraLocker
+ * @author  Aj Maz
+ * @notice  The core implementation of graviAuraLockerCore for the protocol owns tokens
+ *          This locker is highly inspired by CVX Locking contract
+ *          Effectively allows for rolling 16 week lockups of CVX, and provides balances available
+ *          at each epoch (1 week). Also receives cvxCrv from `CvxStakingProxy` and redistributes
+ *          to depositors.
+ *
+ */
+
+abstract contract graviAuraLockerCore is
+    ReentrancyGuardUpgradeable,
+    OwnableUpgradeable,
+    igraviAuraLocker
+{
+    /// TODO: Use these functions for initializing
+    /// __ReentrancyGuard_init
+    /// __Ownable_initƒ
+}
